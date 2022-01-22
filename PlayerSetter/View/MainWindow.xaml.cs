0f1e282539252eaad2e35d8629c0309c65b47e36
grafficia-nim(Surface) <nimrua.takashi@grafficia.com>
2022-01-22 15:35:57 +0900

    StreamControlを追加

--- PlayerSetter/View/MainWindow.xaml.cs
+++ PlayerSetter/View/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
@@ -7,31 +6,6 @@ using static PlayerSetter.View.win32;
 
 namespace PlayerSetter.View
 {
-	static class win32
-	{
-
-
-		[StructLayout(LayoutKind.Sequential)]
-		public struct POINT
-		{
-			public int x;
-			public int y;
-			public static implicit operator System.Drawing.Point(POINT point)
-			{
-				return new System.Drawing.Point(point.x, point.y);
-			}
-		}
-		[DllImport("user32.dll", SetLastError = true)]
-		public static extern IntPtr WindowFromPoint(POINT point);
-
-		[DllImport("User32.dll")]
-		public static extern bool GetCursorPos(out POINT lpPoint);
-
-		[DllImport("user32")]
-		public extern static IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
-
-	}
-
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
@@ -41,7 +15,6 @@ namespace PlayerSetter.View
 
 		public MainWindow()
 		{
-
 			InitializeComponent();
 		}
 
@@ -68,5 +41,29 @@ namespace PlayerSetter.View
 
 			MessageBox.Show($"hwnd:[{hWnd}]");
 		}
+	}
+
+
+	static class win32
+	{
+		[StructLayout(LayoutKind.Sequential)]
+		public struct POINT
+		{
+			public int x;
+			public int y;
+			public static implicit operator System.Drawing.Point(POINT point)
+			{
+				return new System.Drawing.Point(point.x, point.y);
+			}
+		}
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern IntPtr WindowFromPoint(POINT point);
+
+		[DllImport("User32.dll")]
+		public static extern bool GetCursorPos(out POINT lpPoint);
+
+		[DllImport("user32")]
+		public extern static IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
 	}
 }
---
+++ PlayerSetter/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerSetter.ViewModel
+{
+	public class MainWindowViewModel
+	{
+		public ReactivePropertySlim<string> targetHandle { get; } = new ReactivePropertySlim<string>(String.Empty);	
+	}
+}
